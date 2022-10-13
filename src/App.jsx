e78457974951f054748a232f2a3a39f065dfcbf3
jhonatan182 <jona.vand98@gmail.com>
2022-10-12 21:47:21 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return <div className="App">hola mundo</div>;
+}
+
+export default App;
