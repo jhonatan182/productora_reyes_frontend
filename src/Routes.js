0d842eff137a92b2fb57cad1a46abf8ef18b4072
calebandrade <vicaa96@gmail.com>
2022-11-12 18:07:03 -0600

    facturacion ruta

--- src/Routes.js
+++ src/Routes.js
@@ -1,26 +1,12 @@
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes as Switch, Navigate } from 'react-router-dom';
-import GetData from './Views/Inventario/TablaInventario';
-import LoginInUx from './Views/Login/LoginUX';
-import GetProveedores from './Views/Proveedores/TablaProveedores';
-import Menu from './Views/Menu';
-import ProductosMasVendidos from './Views/Reportes/productosMasVendidos';
-import ClientesCompras from './Views/Reportes/clienteCompra';
-import VentasEmpleados from './Views/Reportes/ventaPorEmpleado';
-import StockProductos from './Views/Reportes/stockProductos';
+import FacturacionUX from './Views/Facturacion/FacturacionUX';
 
 const Routes = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<LoginInUx />} />
-        <Route path="/productos" element={<GetData />} />
-        <Route path="/proveedores" element={<GetProveedores />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/reportes/productosMasVendidos" element={<ProductosMasVendidos />} />
-        <Route path="/reportes/comprasClientes" element={<ClientesCompras />} />
-        <Route path="/reportes/ventasEmpleados" element={<VentasEmpleados />} />
-        <Route path="/reportes/stockProductos" element={<StockProductos />} />
+        <Route path="/facturacion" element={<FacturacionUX />} />
       </Switch>
     </Router>
   );
